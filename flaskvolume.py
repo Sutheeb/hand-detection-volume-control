36611258f4c3652f7f315ef4d14efa83f38f4e4f
Sutheeb <124978224+Sutheeb@users.noreply.github.com>
2023-12-02 22:19:08 +0530

    Add files via upload

---
+++ flaskvolume.py
@@ -0,0 +1,79 @@
+
+
+from flask import Flask, render_template, Response
+import cv2
+import mediapipe as mp
+from math import hypot
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+import numpy as np
+import threading
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+def hand_tracking():
+    cap = cv2.VideoCapture(0)
+
+    mpHands = mp.solutions.hands
+    hands = mpHands.Hands()
+    mpDraw = mp.solutions.drawing_utils
+
+    devices = AudioUtilities.GetSpeakers()
+    interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+    volume = cast(interface, POINTER(IAudioEndpointVolume))
+    volMin, volMax = volume.GetVolumeRange()[:2]
+
+    volbar = 400
+    volper = 0
+
+    while True:
+        success, img = cap.read()
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+        results = hands.process(imgRGB)
+
+        lmList = []
+        if results.multi_hand_landmarks:
+            for handlandmark in results.multi_hand_landmarks:
+                for id, lm in enumerate(handlandmark.landmark):
+                    h, w, _ = img.shape
+                    cx, cy = int(lm.x * w), int(lm.y * h)
+                    lmList.append([id, cx, cy])
+                mpDraw.draw_landmarks(img, handlandmark, mpHands.HAND_CONNECTIONS)
+
+        if lmList != []:
+            x1, y1 = lmList[4][1], lmList[4][2]
+            x2, y2 = lmList[8][1], lmList[8][2]
+
+            cv2.circle(img, (x1, y1), 13, (255, 0, 0), cv2.FILLED)
+            cv2.circle(img, (x2, y2), 13, (255, 0, 0), cv2.FILLED)
+            cv2.line(img, (x1, y1), (x2, y2), (255, 0, 0), 3)
+
+            length = hypot(x2 - x1, y2 - y1)
+            vol = np.interp(length, [30, 350], [volMin, volMax])
+            volbar = np.interp(length, [30, 350], [400, 150])
+            volper = np.interp(length, [30, 350], [0, 100])
+
+            print(vol, int(length))
+            volume.SetMasterVolumeLevel(vol, None)
+
+            cv2.rectangle(img, (50, 150), (85, 400), (0, 0, 255), 4)
+            cv2.rectangle(img, (50, int(volbar)), (85, 400), (0, 0, 255), cv2.FILLED)
+            cv2.putText(img, f"{int(volper)}%", (10, 40), cv2.FONT_ITALIC, 1, (0, 255, 98), 3)
+
+        ret, jpeg = cv2.imencode('.jpg', img)
+        frame = jpeg.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(hand_tracking(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(debug=True)
